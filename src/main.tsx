4e6b07036011354af8ff6b6159e42bef2c5bfd8d
Artur Signell <artur@vaadin.com>
2024-06-24 11:15:38 +0300

    Test using React 19

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import { installAutoSignalTracking } from '@preact/signals-react/runtime';
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
+installAutoSignalTracking();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
